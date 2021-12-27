8bfd70a4f4c4d1df932f63ae9d31f442fc3bfec3
infosergio2020 <infosergio2020@gmail.com>
2021-12-27 16:00:07 -0300

    se cambio SQL_ALCHEMY_DATABASE_URI  por   SQLALCHEMY_DATABASE_URI por que no hacia la configuracion de la URI para conectarse con el server de MYSQL

--- app.py
+++ app.py
@@ -2,21 +2,24 @@
 from flask import Flask
 from routes.user import user #quiero probar la ruta de usuario
 from routes.evento import evento
-from flask_sqlalchemy import SQLAlchemy #para la configuracion de la app
+from flask_sqlalchemy import SQLAlchemy
 
-##Configuracion del app
-app= Flask(__name__)
-# from flask_marshmallow import Marshmallow
-# from werkzeug.exceptions import MethodNotAllowed
+class MyApp:
+    def __init__(self, ):
+        ##Configuracion del app
+        self._app= Flask(__name__)
+        # from flask_marshmallow import Marshmallow
+        # from werkzeug.exceptions import MethodNotAllowed
+        self._app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:admin123@127.0.0.1:3306/eventos'
+        self._app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+        SQLAlchemy(self._app) #le paso al ORM la configuracion que posee el self._app
+        #Configuracion del self._app
 
-app.config['SQL_ALCHEMY_DATABASE_URI']='mysql://root:admin123@localhost/eventos'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-SQLAlchemy(app) #le paso al ORM la configuracion que posee el app
-#Configuracion del app
-
-app.register_blueprint(user) #invoco las rutas del usuario
-app.register_blueprint(evento)
-
+        self._app.register_blueprint(user) #invoco las rutas del usuario
+        self._app.register_blueprint(evento)
+    
+    def get_app(self):
+        return self._app
 
 
 
