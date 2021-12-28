b9d5860a3adafabd2763641bf68606162908b5e3
infosergio2020 <infosergio2020@gmail.com>
2021-12-27 22:38:04 -0300

    se agregaron funciones en el modelo para realizar las operaciones crud de la BD, falta probar su funcionamiento

--- models/user.py
+++ models/user.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from utils.db import db
 
 class User(db.Model):
@@ -10,3 +11,74 @@ class User(db.Model):
         self.name=name
         self.password=password
         self.email=email
+
+    def set_password(self, password):
+        """settea la contraseña generando un hash para que no se pueda ver"""
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        """desencripta la contraseña y la compara, retorna true si coinciden"""
+        return check_password_hash(self.password, password)
+
+    def save(self):
+        """ se agrega a la base de datos"""
+        if not self.iduser:
+            db.session.add(self)
+        db.session.commit()
+
+    def remove(self):
+        """ se elimina de la base de datos"""
+        if self.iduser:
+            db.session.delete(self)
+            db.session.commit()
+
+    def __repr__(self):
+        """ retorna un string que describe el objeto """
+        return f'<User {self.email}>'
+
+    #  METODOS STATICOS NO REQUIEREN INSTANCIA PARA USARLOS
+
+    @staticmethod
+    def update(id, name, password, email ):
+        """ permite actualizar este objeto en la bd , retorna el usuario que actualizó sino retorna None"""
+        user = User.query.get(id) #buscar en la bd
+        if user:
+            user.name = name
+            user.email = email
+            if password:
+                user.set_password(password)
+            user.save()
+            return user
+        return None
+
+    @staticmethod
+    def delete(id):
+        """ dado un id los busca y si existe lo eliminar, retorna el usuario que eliminó sino retorna None"""
+        user = User.query.get(id)
+        if user:
+            user.remove()
+            return user
+        return None
+
+    @staticmethod
+    def search(search_query, page, per_page):
+        """ busca un usuario por name"""
+        query = User.query
+        if (search_query):
+            query = query.filter(User.name.like(f"%{search_query}%"))
+        return query.paginate(page=page, per_page=per_page, error_out=False)
+
+    @staticmethod
+    def all():
+        """trae a todos los usuarios"""
+        return User.query.all()
+
+    @staticmethod
+    def get_by_id(id):
+        """ busca usuario por id"""
+        return User.query.get(id)
+
+    @staticmethod
+    def find_by_email(email):
+        """ busca un usuario por email """
+        return User.query.filter_by(email = email).first()
