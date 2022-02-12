2a1fd091b39c7346ec7e85a3decfe096b7795863
ori <ori_larusa@hotmail.com>
2022-02-11 23:44:31 -0300

    agregando mas html

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def Index():
     return render_template('index.html')
 
 @app.route('/testimonio1')
-def testimonio1():
+def Testimonio1():
     
     return render_template('/testimonio1.html')    
 
@@ -29,7 +29,42 @@ def Testimonio2():
 @app.route('/testimonio3.html')
 def Testimonio3():
     
-    return render_template('/testimonio3.html')            
+    return render_template('/testimonio3.html')   
+
+@app.route('/water-level-simulator.html')
+def Waterlevelsimulator():
+    
+    return render_template('/water-level-simulator.html')   
+
+
+@app.route('/the-news-of-the-day.html')
+def Thenewsoftheday():
+    
+    return render_template('/the-news-of-the-day.html')   
+
+
+@app.route('/Why-do-we-flood.html')
+def Whydoweflood():
+    
+    return render_template('/Why-do-we-flood.html')         
+
+@app.route('/recomendacion-como-actuar.html')
+def Recomendacioncomoactuar():
+    
+    return render_template('/recomendacion-como-actuar.html')         
+
+
+@app.route('/sandbox.html')
+def Sandbox():
+    
+    return render_template('/sandbox.html')         
+
+
+@app.route('/shocking-photos.html')
+def Shockingphotos():
+    
+    return render_template('/shocking-photos.html')         
+
 # app.secret_key="secret key" # configuro un valor para que se genere una sesion
 # 
 # SQLAlchemy(app) #le paso al ORM la configuracion que posee el app  #Configuracion del app
