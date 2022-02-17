307e6e87437bf05a5f62c51be60afdf66d361c20
lau-yt <laura.y.lp96@gmail.com>
2022-02-17 15:44:30 -0300

    Corregido 1/2 error de navbar
    Corregir para los narradores: solo toma el primer elemento. Acceder por teclado a la pagina funciona.

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,85 @@
-window.addEventListener("DOMContentLoaded",()=>{document.getElementById("logo").addEventListener("click",()=>toggleMenu()),document.getElementById("close-sideBar").addEventListener("click",()=>toggleMenu()),document.getElementById("close-sideBar").addEventListener("keydown",e=>onkey_tab(e)),document.getElementById("covid")&&document.getElementById("covid").focus(),document.getElementById("anounce")&&document.getElementById("anounce").focus(),document.getElementById("about")&&document.getElementById("about").focus(),document.getElementById("languaje")&&document.getElementById("languaje").focus(),document.getElementById("header")&&document.getElementById("header").focus();var e=document.getElementById("lightgallery");null!=e&&e.addEventListener("lgAfterOpen",e=>{alert("lgAfterAppendSlide"),document.getElementById("lg-prev-1").focus()}),null!=e&&lightGallery(e,{download:!1,speed:500,addClass:"lg-custom-thumbnails",plugins:[lgZoom,lgThumbnail,lgVideo],mobileSettings:{showCloseIcon:!0}});let t=document.getElementsByClassName("gallery__libro");if(null!=t&&0!=t.length&&t[0].addEventListener("lgAfterOpen",e=>{alert("lgAfterAppendSlide"),document.getElementById("lg-prev-1").focus()}),null!=t)for(let e=0;e<t.length;e++)lightGallery(t[e],{download:!1,speed:500,addClass:"lg-custom-thumbnails",plugins:[lgThumbnail,lgVideo],mobileSettings:{showCloseIcon:!0}})});const onkey_tab=e=>{switch(e.keyCode){case 13:case 27:document.getElementsByClassName("sidebar-body-ul")[0].setAttribute("tabindex",-1),document.getElementsByClassName("sidebar-body-ul")[0].blur(),toggleMenu();break;case 9:document.getElementById("close-sideBar").blur(),document.getElementsByClassName("sidebar-body-ul")[0].setAttribute("tabindex",1),document.getElementsByClassName("sidebar-body-ul")[0].focus()}},toggleMenu=()=>{document.getElementById("menuIcono").classList.toggle("active"),document.getElementById("logo").classList.toggle("boton-active");let e=document.getElementsByClassName("sidebar-body-ul")[0].children;for(let t=0;t<e.length;t++)aux=t+1,-1==e[t].getAttribute("tabindex")?e[t].setAttribute("tabindex",aux):e[t].setAttribute("tabindex",-1);-1!=e[0].getAttribute("tabindex")?(e[0].focus(),document.getElementById("close-sideBar").setAttribute("tabindex",e.length+1)):(document.getElementById("close-sideBar").setAttribute("tabindex",-1),document.getElementById("close-sideBar").blur())};
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("logo").addEventListener("click", () => toggleMenu()),
+    document
+      .getElementById("close-sideBar")
+      .addEventListener("click", () => toggleMenu()),
+    document
+      .getElementById("close-sideBar")
+      .addEventListener("keydown", (e) => onkey_tab(e)),
+    document.getElementById("covid") &&
+      document.getElementById("covid").focus(),
+    document.getElementById("anounce") &&
+      document.getElementById("anounce").focus(),
+    document.getElementById("about") &&
+      document.getElementById("about").focus(),
+    document.getElementById("languaje") &&
+      document.getElementById("languaje").focus(),
+    document.getElementById("header") &&
+      document.getElementById("header").focus();
+  var e = document.getElementById("lightgallery");
+  null != e &&
+    e.addEventListener("lgAfterOpen", (e) => {
+      alert("lgAfterAppendSlide"), document.getElementById("lg-prev-1").focus();
+    }),
+    null != e &&
+      lightGallery(e, {
+        download: !1,
+        speed: 500,
+        addClass: "lg-custom-thumbnails",
+        plugins: [lgZoom, lgThumbnail, lgVideo],
+        mobileSettings: { showCloseIcon: !0 },
+      });
+  let t = document.getElementsByClassName("gallery__libro");
+  if (
+    (null != t &&
+      0 != t.length &&
+      t[0].addEventListener("lgAfterOpen", (e) => {
+        alert("lgAfterAppendSlide"),
+          document.getElementById("lg-prev-1").focus();
+      }),
+    null != t)
+  )
+    for (let e = 0; e < t.length; e++)
+      lightGallery(t[e], {
+        download: !1,
+        speed: 500,
+        addClass: "lg-custom-thumbnails",
+        plugins: [lgThumbnail, lgVideo],
+        mobileSettings: { showCloseIcon: !0 },
+      });
+});
+const onkey_tab = (e) => {
+    switch (e.keyCode) {
+      case 13:
+      case 27:
+        document
+          .getElementsByClassName("sidebar-body-ul")[0]
+          .setAttribute("tabindex", -1),
+          document.getElementsByClassName("sidebar-body-ul")[0].blur(),
+          toggleMenu();
+        break;
+      case 9:
+        document.getElementById("close-sideBar").blur(),
+          document
+            .getElementsByClassName("sidebar-body-ul")[0]
+            .setAttribute("tabindex", 1),
+          document.getElementsByClassName("sidebar-body-ul")[0].focus();
+    }
+  },
+  toggleMenu = () => {
+    document.getElementById("menuIcono").classList.toggle("active"),
+      document.getElementById("logo").classList.toggle("boton-active");
+    let e = document.getElementsByClassName("sidebar-body-ul")[0].children;
+    for (let t = 0; t < e.length; t++)
+      (aux = t + 1),
+        -1 == e[t].children[0].getAttribute("tabindex")
+          ? e[t].children[0].setAttribute("tabindex", aux)
+          : e[t].children[0].setAttribute("tabindex", -1);
+    -1 != e[0].children[0].getAttribute("tabindex")
+      ? (e[0].children[0].focus(),
+        document
+          .getElementById("close-sideBar")
+          .setAttribute("tabindex", e.length + 1))
+      : (document.getElementById("close-sideBar").setAttribute("tabindex", -1),
+        document.getElementById("close-sideBar").blur());
+  };
