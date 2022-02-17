e39da857f39b254535b94c6c802097a0a87f25ab
lau-yt <laura.y.lp96@gmail.com>
2022-02-17 17:46:15 -0300

    Corregido 2/2 navbar. Con tab + enter ingresas
    Esperar a que surjan ideas para ver como entrar con enter unicamente

--- static/js/index.js
+++ static/js/index.js
@@ -70,16 +70,6 @@ const onkey_tab = (e) => {
     document.getElementById("menuIcono").classList.toggle("active"),
       document.getElementById("logo").classList.toggle("boton-active");
     let e = document.getElementsByClassName("sidebar-body-ul")[0].children;
-<<<<<<< HEAD
-    for (let t = 0; t < e.length; t++)
-      (aux = t + 1),
-        -1 == e[t].children[0].getAttribute("tabindex")
-          ? e[t].children[0].setAttribute("tabindex", aux)
-          : e[t].children[0].setAttribute("tabindex", -1);
-         
-    -1 != e[0].children[0].getAttribute("tabindex")
-      ? (e[0].children[0].focus(),
-=======
     for (let t = 0; t < e.length; t++){
         aux = t + 1;
         if (-1 == e[t].children[0].getAttribute("tabindex") )
@@ -88,7 +78,6 @@ const onkey_tab = (e) => {
     }
     if (-1 != e[0].children[0].getAttribute("tabindex"))
       { (e[0].focus(),
->>>>>>> 281c56e4029562e71c46dc70a8470fcb61703291
         document
           .getElementById("close-sideBar")
           .setAttribute("tabindex", e.length + 1)) }
