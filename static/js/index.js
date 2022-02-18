070f273cda48cd8f39b1c2419e50a89884991152
lau-yt <laura.y.lp96@gmail.com>
2022-02-17 22:29:30 -0300

    sweetalert2 agregados al diario
    funciona con el narrador pero hay que ver que pasa en la version movil que ni siquiera accede a los links

--- static/js/index.js
+++ static/js/index.js
@@ -83,14 +83,14 @@ const onkey_tab = (e) => {
     else (document.getElementById("close-sideBar").setAttribute("tabindex", -1),
         document.getElementById("close-sideBar").blur());
   };
-
+/**
+ * A partir de acá se utiliza sweetalert2 para mostrar advertencias antes de salir de diarios
+ */
   var e = document.getElementById('diario-1');
   e.addEventListener("click", (e) => {
     Swal.fire({
-      title: '<strong>Atención</strong>',
-      icon: 'info',
-      html:
-        'Estas saliendo de la página de Citadine, ¿Desea continuar?',
+      title: '<strong>Atención. </strong>Estas saliendo de la página de Citadine hacia un enlace del diario El Día. ¿Desea continuar?',
+      icon: 'warning',
       showCloseButton: true,
       showDenyButton: true,
       focusConfirm: false,
@@ -100,5 +100,59 @@ const onkey_tab = (e) => {
       denyButtonAriaLabel: 'Cancelado.'
     }).then((result) => {
         if (result.isConfirmed) window.location.href='https://www.eldia.com/nota/2013-4-3--el-agua-ya-esta-en-un-metro-y-medio-por-favor-que-alguien-nos-mande-botes'
+    })
+  });
+
+  e = document.getElementById('diario-2');
+  null != e &&
+  e.addEventListener("click", (e) => {
+    Swal.fire({
+      title: '<strong>Atención.</strong>Estas saliendo de la página de Citadine hacia un enlace del diario Hoy. ¿Desea continuar?',
+      icon: 'warning',
+      showCloseButton: true,
+      showDenyButton: true,
+      focusConfirm: false,
+      confirmButtonText: 'Aceptar',
+      denyButtonText: 'Cancelar',
+      confirmButtonAriaLabel: 'Saliendo de la página.',
+      denyButtonAriaLabel: 'Cancelado.'
+    }).then((result) => {
+        if (result.isConfirmed) window.location.href='https://diariohoy.net/adjuntos/archivos/000/024/0000024100.pdf'
+    })
+  });
+
+  e = document.getElementById('diario-3');
+  null != e &&
+  e.addEventListener("click", (e) => {
+    Swal.fire({
+      title: '<strong>Atención.</strong>Estas saliendo de la página de Citadine hacia un enlace del diario Clarín. ¿Desea continuar?',
+      icon: 'warning',
+      showCloseButton: true,
+      showDenyButton: true,
+      focusConfirm: false,
+      confirmButtonText: 'Aceptar',
+      denyButtonText: 'Cancelar',
+      confirmButtonAriaLabel: 'Saliendo de la página.',
+      denyButtonAriaLabel: 'Cancelado.'
+    }).then((result) => {
+        if (result.isConfirmed) window.location.href='https://www.clarin.com/ciudades/critica-situacion-plata-cientos-evacuados_0_S19zAYFiDXg.html'
+    })
+  });
+
+  e = document.getElementById('diario-4');
+  null != e &&
+  e.addEventListener("click", (e) => {
+    Swal.fire({
+      title: '<strong>Atención.</strong> Estas saliendo de la página de Citadine hacia un enlace del diario La Nación. ¿Desea continuar?',
+      icon: 'warning',
+      showCloseButton: true,
+      showDenyButton: true,
+      focusConfirm: false,
+      confirmButtonText: 'Aceptar',
+      denyButtonText: 'Cancelar',
+      confirmButtonAriaLabel: 'Saliendo de la página.',
+      denyButtonAriaLabel: 'Cancelado.'
+    }).then((result) => {
+        if (result.isConfirmed) window.location.href='https://es.kiosko.net/ar/2013-04-03/np/nacion.html'
     })
   });
