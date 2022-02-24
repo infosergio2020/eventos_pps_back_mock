953df99f8a99753fa2a5854cbb0a8b83ad8486d8
infosergio2020 <infosergio2020@gmail.com>
2022-02-24 14:06:33 -0300

    se deshabilito la fuente de google para mejorar performance

--- static/js/acordeon.js
+++ static/js/acordeon.js
@@ -1,1 +1,25 @@
-"use strict";class Accordion{constructor(t){this.rootEl=t,this.buttonEl=this.rootEl.querySelector("button[aria-expanded]");const e=this.buttonEl.getAttribute("aria-controls");this.contentEl=document.getElementById(e),this.open="true"===this.buttonEl.getAttribute("aria-expanded"),this.buttonEl.addEventListener("click",this.onButtonClick.bind(this))}onButtonClick(){this.toggle(!this.open)}toggle(t){t!==this.open&&(this.open=t,this.buttonEl.setAttribute("aria-expanded",`${t}`),t?(this.contentEl.removeAttribute("hidden"),console.log(this.contentEl.children[0].children[0].children),this.contentEl.children[0].children[0].children[0].focus()):this.contentEl.setAttribute("hidden",""))}open(){this.toggle(!0)}close(){this.toggle(!1)}}const accordions=document.querySelectorAll(".accordion");accordions.forEach(t=>{new Accordion(t)});
+"use strict";
+class Accordion {
+    constructor(t) {
+        this.rootEl = t, this.buttonEl = this.rootEl.querySelector("button[aria-expanded]");
+        const e = this.buttonEl.getAttribute("aria-controls");
+        console.log(e);
+        this.contentEl = document.getElementById(e), this.open = "true" === this.buttonEl.getAttribute("aria-expanded"), this.buttonEl.addEventListener("click", this.onButtonClick.bind(this))
+    }
+    onButtonClick() {
+        this.toggle(!this.open)
+    }
+    toggle(t) {
+        t !== this.open && (this.open = t, this.buttonEl.setAttribute("aria-expanded", `${t}`), t ? (this.contentEl.removeAttribute("hidden"), console.log(this.contentEl.children[0].children[0].children), this.contentEl.children[0].children[0].children[0].focus()) : this.contentEl.setAttribute("hidden", ""))
+    }
+    open() {
+        this.toggle(!0)
+    }
+    close() {
+        this.toggle(!1)
+    }
+}
+const accordions = document.querySelectorAll(".accordion");
+accordions.forEach(t => {
+    new Accordion(t)
+});
